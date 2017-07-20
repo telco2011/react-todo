c29548b0ca3722f09a85412da560e8ddb0801fbc
kratos <davidlopez.david@gmail.com>
2017-07-20 10:51:09 +1000

    Initial commit

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config) {
+  config.set({
+    // Start these browsers, currently available:
+      // - Chrome
+      // - ChromeCanary
+      // - Firefox
+      // - Opera
+      // - Safari (only Mac)
+      // - PhantomJS
+      // - IE (only Windows)  
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/foundation-sites/dist/foundation.min.js',
+      'app/tests/**/*.test.jsx'
+    ],
+    preprocessors: {
+        'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
