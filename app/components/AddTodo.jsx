d2c9e330249110ea3b5e03835cd989e4d19e58ed
kratos <davidlopez.david@gmail.com>
2017-07-20 18:39:56 +1000

    Add AddTodo functionality

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var TodoList = require('TodoList');
+
+var AddTodo = React.createClass({
+  onSubmit: function (event) {
+    event.preventDefault();
+    var todoText = this.refs.todoText.value;
+
+    if (todoText.length > 0) {
+      this.refs.todoText.value = '';
+      this.props.onAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus();
+    }
+  },
+  render: function () {
+    return (
+      <div>
+        <form ref="form" onSubmit={this.onSubmit}>
+          <input type="text" ref="todoText" placeholder="What do you need to do?"/>
+          <button className="button expanded">Add TODO</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = AddTodo;
