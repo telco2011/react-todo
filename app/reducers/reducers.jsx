86fc06309559edd12a7aaa33840c1ffc84ef016b
kratos <davidlopez.david@gmail.com>
2017-07-22 17:27:40 +1000

    Add more reducers

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,51 @@
+var UUID = require('node-uuid');
+var moment = require('moment');
+
+export var searchTextReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_SEARCH_TEXT':
+      return action.searchText;
+    default:
+      return state;
+  }
+};
+
+export var showCompletedReducer = (state = false, action) => {
+  switch (action.type) {
+    case 'TOGGLE_SHOW_COMPLETED':
+      return !state;
+    default:
+      return state
+  }
+};
+
+export var todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+    // add case for TOGGLE_TODO completed equal to opposite value & updateCompletedAt
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          };
+        }
+      });
+    default:
+      return state;
+  }
+};
---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,83 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+
+  describe('searchTextReducer', () => {
+
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+
+  });
+
+  describe('showCompletedReducer', () => {
+
+    it('should toggle showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(true), df(action));
+
+      expect(res).toEqual(false);
+    });
+
+  });
+
+  describe('todosReducer', () => {
+
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Wald de dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    // defined todos array with realistic todo item
+    // generate action
+    // call reducer and assert completed flipped
+    it('should toggle todo', () => {
+      var todos = [{
+        id: 1,
+        text: 'First TODO',
+        completed: false,
+        createdAt: 12,
+        completedAt: undefined
+      },{
+        id: 2,
+        text: 'Second TODO',
+        completed: false,
+        createdAt: 12,
+        completedAt: undefined
+      },{
+        id: 3,
+        text: 'Third TODO',
+        completed: false,
+        createdAt: 12,
+        completedAt: undefined
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 2
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res.length).toEqual(3);
+      expect(res[1].completed).toEqual(true);
+      expect(res[1].completedAt).toExist();
+    });
+
+  });
+
+});
