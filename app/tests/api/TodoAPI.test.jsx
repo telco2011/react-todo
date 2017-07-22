98cb6ce0f8eb55220fa9b4bbf309b9e52b00980c
kratos <davidlopez.david@gmail.com>
2017-07-22 10:31:56 +1000

    Add filtered search

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -28,7 +28,7 @@ describe('TodoAPI', () => {
 
         expect(actualTodos).toEqual(todos);
       } catch (error) {
-        expect().fail();
+        expect().fail(error);
       }
     });
 
@@ -60,6 +60,36 @@ describe('TodoAPI', () => {
       var actualTodos = TodoAPI.getTodos();
 
       expect(actualTodos).toEqual(todos);
+    });
+
+  });
+
+  describe('filteredTodos', () => {
+
+    var todos = [{
+      id: 1,
+      text: 'Some text here',
+      completed: true
+    },{
+      id: 2,
+      text: 'Some text here',
+      completed: false
+    },{
+      id: 3,
+      text: 'Some text here',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos when showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
     });
 
   });
