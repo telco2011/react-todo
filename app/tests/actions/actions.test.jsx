e27a394a18fc63789984b8ad8e4d2e8b6bbd6e15
kratos <davidlopez.david@gmail.com>
2017-07-24 18:09:28 +1000

    Fix tests again with mock store

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,5 +1,10 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 var expect = require('expect');
+
 var actions = require('actions');
+
+var createMockStore = configureMockStore([thunk]);
 
 describe('Actions', () => {
 
@@ -26,6 +31,19 @@ describe('Actions', () => {
     var res = actions.addTodo(action.todo);
 
     expect(res).toEqual(action);
+  });
+
+  it('should create todo and dispatch ADD_TODO', (done) => {
+    const store = createMockStore({});
+    const todoText = 'My todo item';
+
+    store.dispatch(actions.startAddTodo(todoText)).then(() => {
+      const actions = store.getActions();
+
+      expect(actions[0]).toInclude({type: 'ADD_TODO'});
+      expect(actions[0].todo).toInclude({text: todoText});
+      done();
+    }).catch(done);
   });
 
   it('should generate add todos action object', () => {
