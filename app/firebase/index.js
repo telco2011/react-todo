d88de83c65d266e4c62888868a3f6a458d0bd8da
kratos <davidlopez.david@gmail.com>
2017-07-25 18:09:38 +1000

    Move firebase configuration

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyAj1rFZNkdd86AVTRRPyi6zAaQUBZ74z8w",
-    authDomain: "david-todo-app-a2782.firebaseapp.com",
-    databaseURL: "https://david-todo-app-a2782.firebaseio.com",
-    projectId: "david-todo-app-a2782",
-    storageBucket: "david-todo-app-a2782.appspot.com",
-    messagingSenderId: "26723724647"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
