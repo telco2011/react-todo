412dd5369f565802cc0012b5cb6824add188bc5b
kratos <davidlopez.david@gmail.com>
2017-07-25 10:53:10 +1000

    Fetch todos from firebase on app start

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -22,6 +22,26 @@ export var addTodos = (todos) => {
   };
 };
 
+export var startAddTodos = () => {
+  return (dispatch, getState) => {
+    var todosRef = firebaseRef.child('todos');
+
+    return todosRef.once('value').then((snapshot) => {
+      var todos = snapshot.val() || {};
+      var parsedTodos = [];
+
+      Object.keys(todos).forEach((todoId) => {
+        parsedTodos.push({
+          id: todoId,
+          ...todos[todoId]
+        });
+      });
+
+      dispatch(addTodos(parsedTodos));
+    });
+  };
+};
+
 export var startAddTodo = (text) => {
   return (dispatch, getState) => {
     var todo = {
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -11,58 +11,7 @@ describe('TodoAPI', () => {
   
   it('should exist', () => {
     expect(TodoAPI).toExist();
-  });
-
-  describe('setTodos', () => {
-
-    it('should set valid todos array', () => {
-      var todos = [{
-        id: 23,
-        text: 'test all files',
-        completed: false
-      }];
-      TodoAPI.setTodos(todos);
-
-      try {
-        var actualTodos = JSON.parse(localStorage.getItem(localStorageKeyTodos));
-
-        expect(actualTodos).toEqual(todos);
-      } catch (error) {
-        expect().fail(error);
-      }
-    });
-
-    it('should not set invalid todos array', () => {
-      var badTodos = {a: 'b'};
-
-      TodoAPI.setTodos(badTodos);
-
-      expect(localStorage.getItem(localStorageKeyTodos)).toBe(null);
-    });
-
-  });
-
-  describe('getTodos', () => {
-    
-    it('should return empty array for bad localstorage data', () => {
-      var actualTodos = TodoAPI.getTodos();
-      expect(actualTodos).toEqual([]);
-    })
-
-    it('should return todo if valid array is localstorage', () => {
-      var todos = [{
-        id: 23,
-        text: 'text all files',
-        completed: false
-      }];
-      localStorage.setItem(localStorageKeyTodos, JSON.stringify(todos));
-      
-      var actualTodos = TodoAPI.getTodos();
-
-      expect(actualTodos).toEqual(todos);
-    });
-
-  });
+  });  
 
   describe('filterTodos', () => {
 
