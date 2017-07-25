e0c173b8472b252b02d56b486cae02497136dbc7
kratos <davidlopez.david@gmail.com>
2017-07-25 19:04:25 +1000

    Add Login and Logout with github

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -15,5 +15,6 @@ try {
   console.log('ERROR', error);
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase;
